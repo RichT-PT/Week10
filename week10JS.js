06839668bccbaf811ddcb33c00ef767eb993cdbc
RichT-PT <diverdwn06@outlook.com>
2023-02-22 16:36:00 -0600

    working through the syntax

--- week10JS.js
+++ week10JS.js
@@ -1,77 +1,37 @@
 
-let button = document.getElementById("submit");
+// let button = document.getElementById("submit");
 
-console.log(button);
-// input
+// console.log(button);
 
-// button.addEventListener('click', () =>{
-
-
-// }); 
-// Get the data table element.
-
-// Create a row using the insertRow method and inset it into the table.
-
-// Create new cell(s) using the insertCell method and insert them into the row you created.
-
-// Add data to the newly created cells.
-// table.insertRow(-1)
-
-// function addRow() {
-//     // Get the table element in which you want to add row
-//     let table = document.getElementById("myTable");
- 
-//     // Create a row using the inserRow() method and
-//     // specify the index where you want to add the row
-//     let row = table.insertRow(-1); // We are adding at the end
- 
-//     // Create table cells
-//     let c1 = row.insertCell(0);
-//     let c2 = row.insertCell(1);
-//     let c3 = row.insertCell(2);
- 
-//     // Add data to c1 and c2
-//     c1.innerText = "Elon"
-//     c2.innerText = 45
-//     c3.innerText = "Houston"
-//  }
-//  Get the table body element in which you want to add a row
-
-// Create row element
-
-// Create cells Insert data into cells
-
-// Append cells to the row
-
-// Append row to table body
-
-// function addRow() {
-//     // Get the table body element in which you want to add row
-//     let table = document.getElementById("tableBody");
- 
-//     // Create row element
-//     let row = document.createElement("tr")
+const tableRows = [
+    {
+        firstName: "James",
+        lastName: "Dunkle",
+        role: "President",
+        certLevel: "PSD",
+    },
+];
+function populateTable() {   // make a row for each member
+    for(const member of tableRows){
+        const row = document.createElement("tr");
+    }
+}
+function createTable() {
+    const table = document.createElement("table");
+    const tableBody = document.createElement("tbody");
     
-//     // Create cells
-//     let c1 = document.createElement("td")
-//     let c2 = document.createElement("td")
-//     let c3 = document.createElement("td")
-//     let c4 = document.createElement("td")
+    // make the columns for each member
+    // add data for each column
+    // add the row to the tableBody
     
-//     // Insert data to cells
-//     c1.innerText = "Elon"
-//     c2.innerText = "42"
-//     c3.innerText = "Houston"
-//     c4.innerText = "C++"
-    
-//     // Append cells to row
-//     row.appendChild(c1);
-//     row.appendChild(c2);
-//     row.appendChild(c3);
-//     row.appendChild(c4);
-    
-//     // Append row to table body
-//     table.appendChild(row)
-//  }
-//let newInfo = [];
+    table.appendChild(tableBody);                // add tableBody to table
+    const root = document.getElementById("root");
+    root.appendChild(table);                     // add table to documents
+}
+// createTable();
+// const collection = document.body.children;
+// let text = "";
+// for(let i = 0; i < collection.length; i++){
+//     text += collection[i].tagName + "<br>";
+// }
 
